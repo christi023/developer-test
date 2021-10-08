1d4c9f9eb808ac0352236af2895d15b3bd07d9e4
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-10-08 13:55:04 +0200

    created table for file data upload

---
+++ src/Components/Table/TableBody.jsx
@@ -0,0 +1,32 @@
+const TableBody = ({ userData }) => {
+  return (
+    <tbody>
+      {userData.map((data, index) => {
+        console.log('mmm', data);
+        return (
+          <tr key={index}>
+            <th scope="row">{data.Rank}</th>
+            <td>{data.CLINIC_NO}</td>
+            <td>{data.BARCODE}</td>
+            <td>{data.PATIENT_ID}</td>
+            <td>{data.PATIENT_NAME}</td>
+            <td>{data.DOB}</td>
+            <td>{data.GENDER}</td>
+            <td>{data.COLLECTIONDATE}</td>
+            <td>{data.COLLECTIONTIME}</td>
+            <td>{data.TESTCODE}</td>
+            <td>{data.TESTNAME}</td>
+            <td>{data.RESULT}</td>
+            <td>{data.UNIT}</td>
+            <td>{data.REFRANGELOW}</td>
+            <td>{data.REFRANGEHIGH}</td>
+            <td>{data.NOTE}</td>
+            <td>{data.NONSPECREFS}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
+
+export default TableBody;
---
+++ src/Components/TableInfo.jsx
@@ -0,0 +1,13 @@
+//import TableHeader from './TableHeader';
+import TableBody from './Components/Table/TableBody';
+import Table from 'react-bootstrap/Table';
+
+const TableInfo = ({ itemList }) => {
+  return (
+    <Table striped bordered hover variant>
+      <TableBody userData={itemList} />
+    </Table>
+  );
+};
+
+export default TableInfo;
