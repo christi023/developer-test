8b215903306709f35970daf02c2583f94885d657
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-10-08 14:48:01 +0200

    holds keys for mapping

---
+++ src/Components/data.js/keys.js
@@ -0,0 +1,19 @@
+const keys = [
+  'CLINIC_NO',
+  'BARCODE',
+  'PATIENT_ID',
+  'PATIENT_NAME',
+  'DOB',
+  'GENDER',
+  'COLLECTIONDATE',
+  'COLLECTIONTIME',
+  'TESTCODE',
+  'TESTNAME',
+  'RESULT',
+  'UNIT',
+  'REFRANGELOW',
+  'REFRANGEHIGH',
+  'NOTE',
+  'NONSPECREFS',
+];
+export default keys;
